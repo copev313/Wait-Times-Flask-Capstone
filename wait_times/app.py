553efc51c8e8a4120f804808d510bc306fa1f8e6
Evan Cope <copev313@gmail.com>
2021-06-08 13:24:59 -0400

    styled signin screen + added header & footer

--- wait_times/app.py
+++ wait_times/app.py
@@ -10,19 +10,38 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
+# Home Screen (for tests):
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# Login Screen:
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 
+# Registration Screen:
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+# Dashboard Screen [protected]:
+@app.route('/dashboard')
+def dashboard(username):
+    username = 'Hashy'
+    return render_template('dashboard.html', username=username)
+
+
+# Ride Data Screen [protected]:
+@app.route('/ride/<name>')
+def ride(name):
+    username = 'Hashy'
+    name = 'RIDE NAME'
+    return render_template('ride.html', username=username, ride_name=name)
+
 
 
 if __name__ == '__main__':
